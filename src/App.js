f2b7418ba85976e5de06fec1ab7181ee8ef7d920
Seongju38 <ljg013@naver.com>
2025-03-17 22:22:32 +0900

    MemoContainer.js 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import MemoContainer from './components/MemoContainer';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <MemoContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MemoContainer.js
@@ -0,0 +1,14 @@
+function MemoContainer() {
+  return (
+    <div className="MemoContainer">
+      <div>
+        <input type="text"></input>
+      </div>
+      <div>
+        <textarea />
+      </div>
+    </div>
+  );
+}
+
+export default MemoContainer;
