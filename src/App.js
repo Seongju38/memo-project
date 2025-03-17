4d3046498289120fd66681dd27baf6b4da84043a
Seongju38 <ljg013@naver.com>
2025-03-17 21:38:09 +0900

    README.md 수정 및 필요 없는 파일, 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //const arr = ["1234564789456454654564546", "1234564789456454654564546", "1234564789456454654564546"] -> ctrl + s
-  const arr = [
-    '1234564789456454654564546',
-    '1234564789456454654564546',
-    '1234564789456454654564546',
-  ];
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
